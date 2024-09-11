18e73fa22a03ea4c36bfca8e155cdfbc3e94cc7e
Santiago Parra <143861114+AsTEKA01@users.noreply.github.com>
2024-09-11 13:12:07 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('America/Lima');
 session_start();
-define('RUTA', 'http://localhost/asistencia/');
-define('DB_HOST', 'localhost');
+define('RUTA', 'mysql://root:IwVBmvAPTVYjgVfWShWKBtZHtMieTeSw@mysql.railway.internal:3306/railway');
+define('DB_HOST', 'mysql.railway.internal');
 define('DB_USER', 'root');
-define('DB_NAME', 'assiscor');
-define('DB_PASS', '');
+define('DB_NAME', 'railway');
+define('DB_PASS', 'IwVBmvAPTVYjgVfWShWKBtZHtMieTeSw');
 ?>
